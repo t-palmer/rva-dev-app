60cde62207a21ef5d4db4d8fcd2347dbd3d243d3
Todd Palmer <tpalmer@us.fujitsu.com>
2018-09-02 18:02:56 -0400

    Using the library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RvaDevLibModule } from 'rva-dev-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RvaDevLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
